6820e9c4d4dc54d119e5da4e0f0537f1f51c1344
Paulo Lima <prmlimajr@hotmail.com>
2021-07-04 23:20:29 -0300

    All tests passed

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,21 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const userAlreadyExists = this.usersRepository.findByEmail(email);
+
+    if (!name || !email) {
+      throw new Error("Provide name and email");
+    }
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    this.usersRepository.create({ name, email });
+
+    const user = this.usersRepository.findByEmail(email);
+
+    return user;
   }
 }
 
